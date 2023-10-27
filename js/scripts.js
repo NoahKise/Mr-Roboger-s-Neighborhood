acd22fb852b7e11acaf1c8d358fb407afaf363dc
noahkise <noah@kisefamily.com>
2023-10-27 09:57:46 -0700

    pass test 5

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,20 @@ function roger(input) {
   console.log(inputStringArray);
   const boopArray = [];
 
+  inputArray.forEach(function(element) {
+    if (element.toString().indexOf("1") > -1) {
+      boopArray.push("beep");
+    } else if (element.toString().indexOf("2") > -1) {
+      boopArray.push("boop");
+    } else if (element.toString().indexOf("3") > -1) {
+      boopArray.push("Won't you be my neighbor?");
+    } else {
+      boopArray.push(element);
+    }
+    console.log(boopArray);
+  })
+}
+
   // forEach loop option
   inputArray.forEach(function (element) {
     if (one.includes(element)) {
