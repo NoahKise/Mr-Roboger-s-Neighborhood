30b0eb44e7a8cc915c583c3a55869b575ebbbcc7
noahkise <noah@kisefamily.com>
2023-10-27 09:37:14 -0700

    pass test 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
 function roger(input) {
+  const one = [1]
   const inputArray = [];
   for (let i = 0; i <= input; i += 1) {
     inputArray.push(i);
   }
-  console.log(inputArray);
-}
+  const inputStringArray = inputArray.map(String);
+  console.log(inputStringArray);
+  const boopArray = [];
+  
+  // forEach loop option
+  inputArray.forEach(function(element) {
+    if (one.includes(element)) {
+      boopArray.push("boop");
+    } else {
+      boopArray.push(element);
+    }
+    console.log(boopArray);
+  })
+  }
+
+  // for loop option
+//   for (let i = 0; i < inputStringArray.length; i += 1) {
+//     if (one.includes(i)) {
+//       boopArray.push("boop");
+//     } else {
+//       boopArray.push(i);
+//     }
+//   }
+//   console.log(boopArray);
+// }
